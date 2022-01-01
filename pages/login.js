8efbd8581ce2f70b2ba59551100d84ff271ce612
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2022-01-01 15:13:58 +0600

    first commit

---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+function Login() {
+    return (
+        <>
+         <Head>
+          <title>Spotify : Login</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        <div>
+            <h1>Hi from login</h1>
+        </div>
+
+        </>
+
+    )
+}
+
+export default Login
